7c17b4ce033477b78e816068a60450a7d5ca41c1
Denisse Marinel Montalvo Castillo <marinel.casti@gmail.com>
2021-04-29 21:36:21 -0500

    FIX: add route signin of the signUp

--- src/view/SignUp.js
+++ src/view/SignUp.js
@@ -1,44 +1,62 @@
 import { createUser } from '../lib/user/userService.js';
 
 export default () => {
-  const viewSignUp = document.createElement('form');
+  const viewSignUp = document.createElement('div');
   viewSignUp.innerHTML = `
     
-    <div class="col-md-4">
-      <label for="validationCustom01F">Full Name</label>
-      <input
-        type="text"
-        id="signUpFullname"
-        value="Mark"
-        required
-      />
-    </div>
-    <div >
-      <label for="floatingInput">Email address</label>
-      <input
-        type="email"
-        id="signUpEmail"
-        placeholder="name@example.com"
-      />
-    </div>
-    <div >
-      <label for="floatingPassword">Password</label>
-      <input 
-        type="password" 
-        id="signUpPassword" 
-        placeholder="Password" 
-      />
-    </div>
-    <div >
-      <label for="floatingPassword">Confirm Password</label>
-      <input
-        type="password"
-        class="form-control"
-        id="singUpConfirmPassword"
-        placeholder="Password"
-      />
-    </div>
-    <div >
+  <div class="col-md-4">
+    <label for="validationCustom01"   class="form-label">Full Name</label>
+    <input
+    type="text"
+    class="form-control"
+    id="signUpFullname"
+    value="Mark"
+    required
+    />
+    
+</div>
+<div class="col-md-4">
+  <label for="validationCustomUsername" class="form-label"
+    >Username</label
+  >
+  <span class="input-group has-validation">
+    <span class="input-group-text" id="inputGroupPrepend">@</span>
+    <input
+      type="text"
+      class="form-control"
+      id="validationCustomUsername"
+      aria-describedby="inputGroupPrepend"
+      required
+    />
+    <!-- <div class="invalid-feedback">Please choose a username.</div> -->
+  </span>
+</div>
+<div class="form-floating">
+  <label for="floatingInput">Email address</label>
+  <input
+    type="email"
+    id="signUpEmail"
+    placeholder="name@example.com"
+  />
+</div>
+<div class="form-floating">
+  <label for="floatingPassword">Password</label>
+  <input
+    type="password"
+    id="signUpPassword"
+    placeholder="Password"
+  />
+</div>
+<div class="form-floating">
+  <label for="floatingPassword">Confirm Password</label>
+  <input
+    type="password"
+    class="form-control"
+    id="floatingPassword"
+    placeholder="Password"
+  />
+</div>
+<div >
     <label for="validationCustomUsername" 
       >Chosse your photo</label
     >
@@ -51,32 +69,31 @@ export default () => {
       />
     </span>
   </div>
-    <div >
-      <div class="form-check">
-        <input
-          type="checkbox"
-          value=""
-          id="invalidCheck"
-          required
-        />
-        <label for="invalidCheck">
-          Agree to terms and conditions
-        </label>
-        <div>
-          You must agree before submitting.
-        </div>
-      </div>
+<div class="col-12">
+  <div class="form-check">
+    <input
+      class="form-check-input"
+      type="checkbox"
+      value=""
+      id="invalidCheck"
+      required
+    />
+    <label class="form-check-label" for="invalidCheck">
+      Agree to terms and conditions
+    </label>
+    <div class="invalid-feedback">
+      You must agree before submitting.
     </div>
-    <div >
-      <a id="buttonRegister" href="#/Home"
-        >REGISTER</a
-      >
-    </div>
-    <div >
-      <a id="buttonSignIn" > Sign in </a>
-    </div>
-  
-    `;
+  </div>
+</div>
+<div class="col-12">
+  <a class="btn btn-primary" id="buttonRegister"  href="#/Home">REGISTER</a>
+</div>
+<div class="col-12">
+  <a id="buttonSignIn" class="btn btn-primary" href="">
+    Sign in
+  </a>
+</div>`;
 
   const signUpEmail = viewSignUp.querySelector('#signUpEmail');
   const signUpPassword = viewSignUp.querySelector('#signUpPassword');
