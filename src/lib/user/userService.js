d4da91aa25c312baef933aa4c9636c5f7df00588
Denisse Marinel Montalvo Castillo <marinel.casti@gmail.com>
2021-05-03 13:13:58 -0500

    UPDATE: update data of user

--- src/lib/user/userService.js
+++ src/lib/user/userService.js
@@ -1,23 +1,50 @@
 // aqui exportaras las funciones que necesites
 
-export const createUser = (fullName, email, password, photo) => {
-  firebase
-    .auth()
-    .createUserWithEmailAndPassword(email, password)
-    .then(() => firebase.auth().currentUser)
-    .then((currentUser) => {
-      console.log(`hola ya me creo`);
-      console.log(currentUser);
-      return currentUser.updateProfile({
-        displayName: fullName,
-        photoURL: photo,
+// export const createUser = (fullName, email, password, photo) => new Promise((resolve, reject) => {
+//   firebase
+//     .auth()
+//     .createUserWithEmailAndPassword(email, password)
+//     .then(() => firebase.auth().currentUser)
+//     .then((currentUser) => {
+//       console.log('hola ya me creo');
+//       console.log(currentUser);
+//       currentUser.updateProfile({
+//         displayName: fullName,
+//         photoURL: photo,
+//       });
+//       resolve(true);
+//     })
+//     .catch((error) => reject(new Error(error)));
+// });
+
+export const createUser = (email, password) => {
+  const user = firebase.auth();
+  // const create = user
+  return user.createUserWithEmailAndPassword(email, password);
+};
+
+export const upDateUser = (displayName, photoURL) => {
+  const user = firebase.auth().currentUser;
+  return user.updateProfile({ displayName, photoURL });
+};
+
+export const uploadFileUser = (file) => {
+  // Create a root reference
+  const storageRef = firebase.storage().ref();
+//  console.log(file.files[0]);
+  const metadata = {
+    contentType: 'image/jpeg',
+  };
+
+  // Upload the file and metadata
+  const uploadTask = storageRef
+    .child(`imgUser/${file.name}`)
+    .put(file, metadata)
+    .then((snapshot) => {
+      console.log(snapshot);
+      snapshot.ref.getDownloadURL().then((downloadURL) => {
+        console.log('File available at', downloadURL);
       });
-      // .then((result)=>{
-      //   console.log("Actualizando mis datos");
-      //   console.log(result);
-      // })
     })
-    .catch((error) => {
-      console.log(error);
-    });
+    .catch((error) => console.log(error))
 };
