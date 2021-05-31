84c6ff15cd4901dcc3eba38b964e6fb9a0edabc9
Brigitte Nikoll <roadclosed24@gmail.com>
2021-05-31 15:47:02 -0500

    Agregando imágenes de ReadMe

--- src/view/posts.js
+++ src/view/posts.js
@@ -50,16 +50,6 @@ export const renderPostUser = (element) => {
             <i  class="fa fa-thumbs-up fa-lg" id="buttonLikePost" data-id=${doc.idPost}></i>
               <button class="buttonCommentPost">Comment</button>
             </div>
-
-
-            <section id="commentContainer">
-              <div class="writteComment">
-                <textarea class="comment" placeholder="Escribe algo!" required></textarea>
-                <button class="publicar" data-id=${doc.idPost}>Publicar</button>
-              </div>
-
-              <div id="${doc.idPost}"></div>
-              </section>
               
               `;
 
